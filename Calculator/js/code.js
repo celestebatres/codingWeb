b70e3e5ea15e4d64731fc7d31c884a129a3e915f
Celeste Batres <celestebatres16@gmail.com>
2021-04-29 20:45:01 -0600

    Parte Lógica Calculator

--- Calculator/js/code.js
+++ Calculator/js/code.js
@@ -1,3 +1,23 @@
-function res(){
+let termina = false;
 
+function insertText(elemID, text) {
+    if(termina){
+        document.getElementById(elemID).innerHTML = text;
+        termina = false;
+    }else{
+        document.getElementById(elemID).innerHTML += text;
+    }
+}
+
+function borra(elemID){
+    let str = document.getElementById(elemID).value;
+    let strN = str.substring(0, str.length - 1);
+    document.getElementById(elemID).innerHTML = strN;
+}
+
+function res(elemID){
+    let op = document.getElementById(elemID).value;
+
+    document.getElementById(elemID).innerHTML = Math.round(eval(op)*100)/100;
+    termina = true;
 }
